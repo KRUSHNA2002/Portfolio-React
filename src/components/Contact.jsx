df15518a5f9296af05b7eec31b5e46b584d7779d
Krushna Waghumbare <waghumbarekrushna143@gmail.com>
2024-01-10 12:59:07 +0530

    home page change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,11 +16,11 @@ const Contact =()=>{
                 <div className="col-md-6 col-sm-12 my-5 ">
                 <iframe title="Unique Title" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15137.97879646537!2d73.836065!3d18.461238!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2eebbb3b91ba9%3A0xdeaf2e5b2b8e3bb!2sSinhgad%20Law%20College!5e0!3m2!1sen!2sin!4v1704866135374!5m2!1sen!2sin" width="100%" height="450" style={{border:"0;"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-6 col-sm-12 my-5">
                     <video width="100%" height="430px" controls>
                         <source src={blog} type="video/mp4"></source>
                     </video>
-                    <div className='text-center text-muted my-4'>
+                    <div className='text-center text-muted my-2'>
                         <h4>All projects explain in short video</h4>
                     </div>
                 </div>
